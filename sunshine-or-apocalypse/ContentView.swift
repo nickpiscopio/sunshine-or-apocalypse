fdd6062240923883e902049d68c1ecd5257cea64
Nick Piscopio <nick.piscopio@gmail.com>
2024-04-29 13:19:19 -0400

    This is the official first commit for Sunshine or Apocalypse with the basic iOS app.

---
+++ sunshine-or-apocalypse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sunshine-or-apocalypse
+//
+//  Created by Nick Piscopio on 4/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sunshine-or-apocalypse/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  sunshine-or-apocalypse
+//
+//  Created by Nick Piscopio on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
